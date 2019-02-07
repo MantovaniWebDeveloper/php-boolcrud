f699752d44c3db49529b5eb617cbd16fc7613f2d
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-07 16:22:36 +0100

    recuperati i valori dal form di crea ospite(senza grafica)e creato sql

---
+++ create/creaOspite.php
@@ -0,0 +1,18 @@
+<form action="http://localhost:8888/php-boolcrud/create/server.php" method="post">
+  <div class="form-group">
+    <input class="form-control"type="text" name="nome" value="nome">
+  </div>
+  <div class="form-group">
+    <input class="form-control"type="text" name="lastname" value="lastname">
+  </div>
+  <div class="form-group">
+    <input class="form-control"type="text" name="dataNascita" value="dataNascita">
+  </div>
+  <div class="form-group">
+    <input class="form-control"type="text" name="tipoDocumento" value="tipoDocumento">
+  </div>
+  <div class="form-group">
+    <input class="form-control"type="text" name="numDocumento" value="numDocumento">
+  </div>
+  <button type="submit" name="button">invia</button>
+</form>
--- create/server.php
+++ create/server.php
@@ -8,5 +8,22 @@
     echo("non sono riuscito a connnettermi!" .$conn->connect_error);
   }
 
-  
+  $nome = $_POST["nome"];
+  $cognome = $_POST["lastname"];
+  $dataNascita = $_POST["dataNascita"];
+  $tipoDocumento = $_POST["tipoDocumento"];
+  $numDocumento = $_POST["numDocumento"];
+
+  //controllo se arrivano i valori
+    var_dump($nome);
+    var_dump($cognome);
+    var_dump($dataNascita);
+    var_dump($tipoDocumento);
+    var_dump($numDocumento);
+    die();
+
+  $sql = "INSERT INTO ospiti (name, lastname, date_of_birth,
+          document_type,document_number)
+          VALUES ($nome, $cognome, $dataNascita,$tipoDocumento,$numeroDocumento)";
+
  ?>
