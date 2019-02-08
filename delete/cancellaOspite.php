a5d01a8315bd02f50ba0872fa939b09d6972a510
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-02-08 11:37:33 +0100

    corretto il nome della tabella nel prepare della delete

--- delete/cancellaOspite.php
+++ delete/cancellaOspite.php
@@ -1,4 +1,8 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL | E_STRICT);
+
     include ("../env.php");
 
     $conn = new Mysqli($servername,$user,$pass,$dbname);
@@ -7,14 +11,16 @@
       echo("non sono riuscito a connnettermi!" .$conn->connect_error);
     }
 
-    $getId = intval($_GET["id"]);
+    $getId = $_GET["id"];
     //echo $getId;
     var_dump($getId);
 
-    $sql = "DELETE FROM opsiti
+    $sql = "DELETE FROM ospiti
             WHERE id= ?";
     $cancellaCliente = $conn->prepare($sql);
-    $cancellaCliente->bind_param('i',$getId);
+    $cancellaCliente->bind_param('s',$getId);
+
+
 
     if($cancellaCliente->execute()) {
       echo "Ok, hai eliminato la scheda cliente!";
